caa6a28abf02570453e7bb71ba41affa34e66b25
smallWu <shylinshine@163.com>
2022-03-07 20:53:13 +0800

    login
    登录功能

--- vue_shop/src/main.js
+++ vue_shop/src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+//导入全局样式表
+import './assets/css/global.css'
+import ElementUI from 'element-ui'
+//引入这个之后才有样式
+import  'element-ui/lib/theme-chalk/index.css'
 
+// 导入弹框提示组件
+import { Message }  from 'element-ui'
+
+Vue.prototype.$http = axios
+//把弹框组件挂在到Vue的原型对象上
+Vue.prototype.$message = Message
+
+//配置请求的根路径
+axios.defaults.baseURL = 'https://lianghj.top:8888/api/private/v1'
 Vue.config.productionTip = false
+
+// 注册
+Vue.use(ElementUI)
 
 new Vue({
   router,
